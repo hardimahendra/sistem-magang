5eb473972e2a883daa943d6da95f5e48b02b589b
Hardi Mahendra <41864461+hardimahendra@users.noreply.github.com>
2023-03-27 15:55:04 +0700

    Update sidebar.jsx

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,34 +1,34 @@
 import React from "react";
-
+import { Link } from 'react-router-dom';
 export function SideBar() {
   return ( 
       <>
     <div className="l-navbar" id="nav-bar">
         <nav className="nav">
             <div> 
-                <a href="/" className="nav_logo"> 
+                <Link to="/" className="nav_logo"> 
                     <img width="25" src="img/unsika-logo.png"/>
                     <span className="nav_logo-name">UNSIKA</span> 
-                </a>
+                </Link>
                 <div className="nav_list">
-                    <a href="/" className="nav_link active">
+                    <Link to="/" className="nav_link active">
                         <i class="fas fa-th-large nav_icon"></i>
                         <span className="nav_name">Dashboard</span> 
-                    </a>
-                    <a href="laporan" className="nav_link">
+                    </Link>
+                    <Link to="laporan" className="nav_link">
                         <i class="fas fa-book nav_icon"></i>
                         <span className="nav_name">Laporan Magang</span> 
-                    </a>
-                    <a href="/daftar" className="nav_link">
+                    </Link>
+                    <Link to="/daftar" className="nav_link">
                         <i class="fa-solid fa-calendar-week nav_icon"></i>
                         <span className="nav_name">Daftar Magang</span> 
-                    </a>
+                    </Link>
                 </div>
             </div> 
-            <a href="/" className="nav_link"> 
+            <Link to="/" className="nav_link"> 
                 <i className='fas fa-sign-out-alt nav_icon'></i> 
                 <span className="nav_name">SignOut</span> 
-            </a>
+            </Link>
         </nav>
     </div>
   </>
